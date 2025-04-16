51e73be49bdd098a6b60325d977dfd1af76d4db8
Spotlight <spotlight@joscomputing.space>
2025-04-16 04:49:29 -0500

    Run Windows Batch script in correct directory

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,2 @@
-python -m pip install -r requirements.txt
-python -m PyInstaller --onefile --noconsole --clean --add-data "layout;layout" --add-data "api;api" --icon=layout\resources\logo.ico --collect-all "pyreadline3" --collect-all "sqlite3" --collect-all "Cryptodome" --collect-all "PIL" --name=3DS-RPC app.py
+python -m pip install -r requirements.txt || exit /b
+python -m PyInstaller --onefile --noconsole --clean --add-data "layout;layout" --add-data "api;api" --icon=layout\resources\logo.ico --collect-all "pyreadline3" --collect-all "sqlite3" --collect-all "Cryptodome" --collect-all "PIL" --name=3DS-RPC app.py || exit /b
